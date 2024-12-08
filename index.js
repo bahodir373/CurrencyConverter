c9b1bea18a67cdc97c55c58f9814086f28600965
bahodir373 <zteqwertyuiop672@gmail.com>
2024-12-08 23:18:13 +0500

    added some changes

--- index.js
+++ index.js
@@ -1,94 +1,94 @@
-const TelegramBot = require('node-telegram-bot-api');
-const axios = require('axios');
-const { startOptions } = require('./options');
+const TelegramBot = require('node-telegram-bot-api')
+const axios = require('axios')
+const { startOptions } = require('./options')
 
-const token = 'YOUR_TELEGRAM_BOT_TOKEN';
+const token = 'YOUR_TELEGRAM_BOT_TOKEN'
 
-const bot = new TelegramBot(token, { polling: true });
+const bot = new TelegramBot(token, { polling: true })
 
 async function getExchangeRate() {
 	try {
 		const response = await axios.get(
 			'https://api.exchangerate-api.com/v4/latest/USD'
 		)
-		const usdToUzs = response.data.rates.UZS;
-		const uzsToUsd = 1 / usdToUzs;
-		return { usdToUzs, uzsToUsd };
+		const usdToUzs = response.data.rates.UZS
+		const uzsToUsd = 1 / usdToUzs
+		return { usdToUzs, uzsToUsd }
 	} catch (error) {
-		console.error('Valyuta kursini olishda xatolik:', error);
-		return null;
+		console.error('Valyuta kursini olishda xatolik:', error)
+		return null
 	}
-};
+}
 
 bot.setMyCommands([
 	{
 		command: '/start',
-		description: "Botni ishga tushirish",
+		description: 'Botni ishga tushirish',
 	},
 	{
 		command: '/convertor',
-		description: "Konvertatsiya qilish",
-	}
-]);
+		description: 'Konvertatsiya qilish',
+	},
+])
 
 bot.on('message', async msg => {
-	const text = msg.text;
-	const chatId = msg.chat.id;
+	const text = msg.text
+	const chatId = msg.chat.id
 
 	if (text === '/start') {
 		await bot.sendMessage(
 			chatId,
 			`Assalomu alaykum ${msg.from.first_name}, bizning valyuta kursini hisoblaydigan botimizga xush kelibsiz!`
-		);
+		)
 
-		return bot.sendMessage(chatId,`Valyuta kursini hisoblash uchun /convertor buyrug'ini ishga tushiring.`)
+		return bot.sendMessage(
+			chatId,
+			`Valyuta kursini hisoblash uchun /convertor buyrug'ini ishga tushiring.`
+		)
 	}
 
 	if (text === '/convertor') {
-		return bot.sendMessage(chatId, `Amalni tanlang:`, startOptions);
+		return bot.sendMessage(chatId, `Amalni tanlang:`, startOptions)
 	}
-
-
-	
-});
+})
 
 bot.on('callback_query', async query => {
-	const chatId = query.message.chat.id;
+	const chatId = query.message.chat.id
 	const data = query.data
 
-	const currencies = await getExchangeRate();
+	const currencies = await getExchangeRate()
 
 	if (!currencies) {
 		return bot.sendMessage(
 			chatId,
 			`Valyuta kursini olishda muammo yuz berdi. Keyinroq urinib ko'ring`
-		);
+		)
 	}
 
 	if (data === 'usdToUzs') {
-		bot.sendMessage(chatId, `Dollar miqdorini kiriting:`);
+		bot.sendMessage(chatId, `Dollar miqdorini kiriting:`)
 
 		bot.once('message', msg => {
-			const amount = parseFloat(msg.text);
+			const amount = parseFloat(msg.text)
 			if (!isNaN(amount)) {
-				const converted = (amount * currencies.usdToUzs).toFixed(2);
-				bot.sendMessage(chatId, `${amount} Dollar = ${converted} So'm`);
+				const converted = (amount * currencies.usdToUzs).toFixed(2)
+				bot.sendMessage(chatId, `${amount} Dollar = ${converted} So'm`)
 			} else {
-				bot.sendMessage(chatId, `Iltimos to'g'ri son kiriting!`);
+				bot.sendMessage(chatId, `Iltimos to'g'ri son kiriting!`)
 			}
-		});
+		})
 	}
 	if (data === 'uzsToUsd') {
-		bot.sendMessage(chatId, `So'm miqdorini kiriting:`);
+		bot.sendMessage(chatId, `So'm miqdorini kiriting:`)
 
 		bot.once('message', msg => {
-			const amount = parseFloat(msg.text);
+			const amount = parseFloat(msg.text)
 			if (!isNaN(amount)) {
-				const converted = (amount * currencies.uzsToUsd).toFixed(2);
-				bot.sendMessage(chatId, `${amount} So'm = ${converted} Dollar`);
+				const converted = (amount * currencies.uzsToUsd).toFixed(2)
+				bot.sendMessage(chatId, `${amount} So'm = ${converted} Dollar`)
 			} else {
-				bot.sendMessage(chatId, `Iltimos to'g'ri son kiriting!`);
+				bot.sendMessage(chatId, `Iltimos to'g'ri son kiriting!`)
 			}
-		});
+		})
 	}
-});
+})
