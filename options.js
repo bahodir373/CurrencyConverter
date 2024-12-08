27a48c853f99ebf8c95567b2f6ce29af06861205
bahodir373 <zteqwertyuiop672@gmail.com>
2024-12-08 23:15:57 +0500

    first nodejs bot

---
+++ options.js
@@ -0,0 +1,10 @@
+module.exports = {
+	startOptions: {
+		reply_markup: {
+        inline_keyboard: [
+            [{ text: `Dollar ➡️ So'm`, callback_data: 'usdToUzs' }],
+            [{ text: `So'm ➡️ Dollar`, callback_data: 'uzsToUsd' }]
+        ]
+    }
+	}
+}
